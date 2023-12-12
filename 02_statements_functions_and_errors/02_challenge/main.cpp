66c47ababe9823e6e99e9f0ad1ad6d06d46ffab5
koki-ota <koki.ota@woven-planet.global>
2023-12-12 17:57:42 +0900

    Initial commit of challenge 02_02

---
+++ 02_statements_functions_and_errors/02_challenge/main.cpp
@@ -0,0 +1,20 @@
+//
+// Created by koki-ota on 23/12/12.
+//
+
+#include <iostream>
+
+int factorial(int n);
+
+int main() {
+    std::cout << "Factorial of 4 is " << factorial(4) << std::endl;
+    return 0;
+}
+
+int factorial(int n) {
+    if (n == 0) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
+}
