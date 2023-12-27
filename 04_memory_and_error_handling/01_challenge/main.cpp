d2ab0b5307e079a5262389b038fac671f6985824
koki-ota <koki.ota@woven-planet.global>
2023-12-27 14:23:27 +0900

    04_01 challenge

---
+++ 04_memory_and_error_handling/01_challenge/main.cpp
@@ -0,0 +1,17 @@
+//
+// Created by koki-ota on 23/12/26.
+//
+
+#include <iostream>
+
+int main() {
+    int x = 42;
+    std::cout << &x << std::endl;
+
+    int* p = &x;
+
+    std::cout << p << std::endl;
+    std::cout << *p << std::endl;
+
+    return 0;
+}
