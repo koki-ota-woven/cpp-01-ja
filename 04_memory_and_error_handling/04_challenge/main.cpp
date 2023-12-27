86a55488a909b4fa461fd86b54c023cd43b3162b
koki-ota <koki.ota@woven-planet.global>
2023-12-27 17:02:02 +0900

    Add 04_04 challenge

---
+++ 04_memory_and_error_handling/04_challenge/main.cpp
@@ -0,0 +1,30 @@
+//
+// Created by koki-ota on 23/12/27.
+//
+
+#include <iostream>
+#include <stdexcept>
+#include <vector>
+
+// コードを入力してください
+int element_at(const std::vector<int>& numbers, int index){
+    if (index < numbers.size()) return numbers[index];
+    throw std::out_of_range("Please input a valid index");
+}
+
+int main() {
+    std::vector<int> numbers = {1, 3, 5, 7, 9};
+    int input_index;
+    std::cout << "Input index \n";
+    std::cin >> input_index;
+
+    // コードを入力してください
+    try {
+        int number = element_at(numbers, input_index);
+        std::cout << "The number at the index is " << number << std::endl;
+    } catch(const std::out_of_range& e){
+        std::cout << e.what() << std::endl;
+    }
+
+    return 0;
+}
